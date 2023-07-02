c0ffecd28f1868b6f5cde0f19f98fedb9d99df43
Fraser Jubb <fraser.jubb@gmail.com>
2023-07-02 21:22:26 +0100

    First Commit. Created Basic shape that can fly. Basic obstacle and landing pads have also been created.

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody rb;
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationSpeed = 150f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessThrust();
+        ProcessRotation();
+    }
+
+    void ProcessThrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            // Debug.Log("Pressed SPACE - Thrusting");
+            rb.AddRelativeForce(0, mainThrust *Time.deltaTime, 0);
+            // Alternative code:
+            // rb.AddRelativeForce(Vector3.up); 
+        }
+    }
+
+    void ProcessRotation()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            // Debug.Log("Pressed A - Rotate Left");
+            ApplyRotation(rotationSpeed);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            // Debug.Log("Pressed D - Rotate Right");
+            ApplyRotation(-rotationSpeed);
+        }
+    }
+
+    void ApplyRotation(float rotationThisFrame)
+    {
+        // First we freeze the physics rotation:
+        rb.freezeRotation = true; // freezing rotation so we can manually rotate.
+        transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
+        rb.freezeRotation = false; // unfreezing rotation so that the physics system can take over.
+    }
+}
