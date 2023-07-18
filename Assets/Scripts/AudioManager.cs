c2d27eb7d659a72ebd02cf5b4de069e4aef1165d
Fraser Jubb <fraser.jubb@gmail.com>
2023-07-18 12:23:15 +0100

    Theme Music now plays seamlessly throughout

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,9 +6,22 @@ public class AudioManager : MonoBehaviour
 {
     public Sound[] sounds;
     
+    public static AudioManager instance;
+
     // Start is called before the first frame update
     void Awake()
     {
+        // to ensure a new audiomanager is not created in the next scene i.e. duplicates.
+        if (instance == null) // we don't have an audio manager in our scene
+            instance = this; // instance = thisObject
+        else // if we already have an instance in our scene
+        {
+            Destroy(gameObject); // destroy this object
+            return; // makes sure no more code is called before we call the object. 
+        }
+        DontDestroyOnLoad(gameObject);
+
+        
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
