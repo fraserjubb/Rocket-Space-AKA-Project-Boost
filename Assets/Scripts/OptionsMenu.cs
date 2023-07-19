e4010ec3300ea11ef1b594e7bb3c2c14ac53129f
Fraser Jubb <fraser.jubb@gmail.com>
2023-07-19 20:32:34 +0100

    Volume slider is now performing but now missing text...

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -2,24 +2,35 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
+    [SerializeField] private Slider volumeSlider = null;
+    
+    
     [SerializeField] private AudioMixer Mixer;
     // [SerializeField] private AudioSource AudioSource;
-    [SerializeField] private TextMeshProUGUI ValueText;
+    [SerializeField] private TextMeshProUGUI volumeTextUI = null;
 
     [SerializeField] private AudioMixMode MixMode;
 
-    public void OnChangeSlider(float Value)
+    private void Start() 
     {
-        ValueText.SetText($"{Value.ToString("N4")}"); // This will show the float value up to four decimal places
+        LoadValues();
+    }
+
+
+    public void OnChangeSlider(float volume)
+    {
+        volumeTextUI.text = volume.ToString("0.0");
+        // volumeTextUI.SetText($"{Value.ToString("N4")}"); // This will show the float value up to four decimal places
 
         switch (MixMode)
         {
             case AudioMixMode.LogrithmicMixerVolume:
-            Mixer.SetFloat("Volume", Mathf.Log10(Value)*20);
+            Mixer.SetFloat("Volume", Mathf.Log10(volume)*20);
             break;
         }
 
@@ -28,5 +39,19 @@ public class OptionsMenu : MonoBehaviour
     public enum AudioMixMode
     {
         LogrithmicMixerVolume
+    }
+
+    public void SaveVolumeButton()
+    {
+        float volumeValue = volumeSlider.value;
+        PlayerPrefs.SetFloat("VolumeValue", volumeValue);
+        LoadValues();
+    }
+
+    void LoadValues()
+    {
+        float volumeValue = PlayerPrefs.GetFloat("VolumeValue");
+        volumeSlider.value = volumeValue;
+        AudioListener.volume = volumeValue;
     }
 }
