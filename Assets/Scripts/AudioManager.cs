62dc505e51cd23b92f0f200ccc5e591d459164d4
Fraser Jubb <fraser.jubb@gmail.com>
2023-07-20 14:27:16 +0100

    Tweaked Sound/AudioManager scripts so that a different mixer can be assigned to each SFX

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -8,7 +8,7 @@ public class AudioManager : MonoBehaviour
     
     public static AudioManager instance;
 
-    public AudioMixerGroup mixerGroup;
+    // public AudioMixerGroup mixerGroup;
 
 
     // Start is called before the first frame update
@@ -28,7 +28,7 @@ public class AudioManager : MonoBehaviour
         foreach (Sound s in sounds)
         {
             s.source = gameObject.AddComponent<AudioSource>();
-            s.source.outputAudioMixerGroup = mixerGroup;
+            s.source.outputAudioMixerGroup = s.mixerGroup;
             s.source.clip = s.clip;
 
             s.source.volume = s.volume;
