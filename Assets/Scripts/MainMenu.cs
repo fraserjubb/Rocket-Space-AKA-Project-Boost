9a143ae38c5fdf4ec580c47fb969717f5051cee2
Fraser Jubb <fraser.jubb@gmail.com>
2023-07-21 13:20:17 +0100

    Refactored MainMenu.cs

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,21 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+//NOTE: This class is currently used on both the Main Menu & Game Complete scenes.
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame()
+    public void PlayGame() // When play button is pressed on the main menu
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(1); // Will load specified scene
     }
 
     // public void Options()
     // {
-    //     Debug.Log("I am the options");
+    //     // Currently controlled through Unity inspector
     // }
 
-    public void QuitGame()
+    public void QuitGame() // When quit button in pressed on the main menu
     {
-        Debug.Log("Game has been quit");
-        Application.Quit();
+        Debug.Log("Game has been quit"); // To show that game is being quit
+        Application.Quit(); // Only works in final build
     }
 }
