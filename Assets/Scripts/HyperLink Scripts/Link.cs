c5419c8dda936f5379d46eedc4a8381bda5a2469
Fraser Jubb <fraser.jubb@gmail.com>
2023-07-21 22:32:15 +0100

    Hyperlink scripts added. Player feedback button working.

---
+++ Assets/Scripts/HyperLink Scripts/Link.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Runtime.InteropServices;
+using UnityEngine.UI;
+
+public class Link : MonoBehaviour 
+{
+
+	public void OpenPlayerFeedbackForm()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://docs.google.com/forms/d/e/1FAIpQLScwQDBHrVtTYs1TSRmxCMhIDjP8cz9FRLt8WkJx_JpeYVQ3Vw/viewform?usp=sf_link");
+		#endif
+		Debug.Log("Player Feedback Form has been opened");
+	}
+
+	public void OpenInstagram()
+	{
+		#if !UNITY_EDITOR
+		openWindow("https://www.instagram.com/thejubbzone/");
+		#endif
+		Debug.Log("The Jubb Zone Insta has been opened");
+	}
+
+	[DllImport("__Internal")]
+	private static extern void openWindow(string url);
+
+}
