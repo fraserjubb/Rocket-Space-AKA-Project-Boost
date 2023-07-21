bdadf6906d4f1b275ae8eabd55cdd5190beb6eb9
Fraser Jubb <fraser.jubb@gmail.com>
2023-07-21 14:46:38 +0100

    Refactored Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
@@ -10,7 +9,7 @@ public class Movement : MonoBehaviour
     [SerializeField] float mainThrust = 1000f;
     [SerializeField] float rotationSpeed = 150f;
 
-    public AudioManager mainEngineAudio;
+    private AudioManager mainEngineAudio;
 
     [SerializeField] ParticleSystem mainEngineParticles;
     [SerializeField] ParticleSystem leftEngineParticles;
@@ -19,20 +18,14 @@ public class Movement : MonoBehaviour
 
     // CACHE - e.g. references in the script for readability or speed
     Rigidbody rb;
-    // AudioSource audioSource;
-    // Physics physics;
+
     // STATE - private instance (member) variables e.g. "bool isAlive"
 
 //START METHOD
     void Start()
-    {
-        
-        // audioSource = GetComponent<AudioSource>();
+    {        
         rb = GetComponent<Rigidbody>();
-        mainEngineAudio = FindObjectOfType<AudioManager>();
-        // physics = GetComponent<Physics>();
-        // audioSource.Play(0);
-        // Debug.Log("started");
+        mainEngineAudio = FindObjectOfType<AudioManager>(); // Searches AudioManager script
     }
 
 //UPDATE METHOD
@@ -40,7 +33,6 @@ public class Movement : MonoBehaviour
     {
         ProcessThrust();
         ProcessRotation();
-        // ReturnToMenu();
     }
 
 // PUBLIC METHODS
@@ -49,10 +41,10 @@ public class Movement : MonoBehaviour
 // PRIVATE METHODS
     void ProcessThrust()
     {
-        if (Input.GetKey(KeyCode.Space) && PauseMenu.gameIsPaused == false && DialogueManager.initialTutorialIsRunning == false) // && Tutorial.initialTutorialIsRunning == false)
+        if (Input.GetKey(KeyCode.Space) && PauseMenu.gameIsPaused == false && DialogueManager.initialTutorialIsRunning == false)
         {
             StartThrusting();
-        } // Make sure to close bracket here as a seperate block = above is if space bar is pressed
+        } 
         else // if space bar is not pressed = below is when space bar is not pressed
         {
             StopThrusting();
@@ -67,10 +59,7 @@ public class Movement : MonoBehaviour
 
     void StartThrusting()
     {
-        // Debug.Log("Pressed SPACE - Thrusting");
-        // rb.AddRelativeForce(0, mainThrust *Time.deltaTime, 0);
-        // Alternative code:
-        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        rb.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime); // Time.deltaTime ensures that it moves at the same speed regardless the framerate of the computer.
         if (!mainEngineParticles.isPlaying) // if audio is not already playing then
         {
             mainEngineAudio.Play("Main Engine SFX");
@@ -95,7 +84,6 @@ public class Movement : MonoBehaviour
 
     void RotatingLeft()
     {
-        // Debug.Log("Pressed A - Rotate Left");        
         ApplyRotation(rotationSpeed);
         if (!rightEngineParticles.isPlaying)
         {
@@ -105,7 +93,6 @@ public class Movement : MonoBehaviour
 
     void RotatingRight()
     {
-        // Debug.Log("Pressed D - Rotate Right");
         ApplyRotation(-rotationSpeed);
         if (!leftEngineParticles.isPlaying)
         {
@@ -125,21 +112,5 @@ public class Movement : MonoBehaviour
         transform.Rotate(Vector3.forward * rotationThisFrame * Time.deltaTime);
         rb.freezeRotation = false; // unfreezing rotation so that the physics system can take over.
     }
-
-    // // void ReturnToMenu()
-    // {
-    //     if (Input.GetKeyDown(KeyCode.Escape))
-    //     {
-    //         SceneManager.LoadScene("Main Menu");
-    //     }
-    // }
-
-    // void TurnOffCollisions()
-    // {
-    //     if (Input.GetKey(KeyCode.C))
-    //     {
-    //         physics.IgnoreCollision();
-    //     }
-    // }
 
 }
