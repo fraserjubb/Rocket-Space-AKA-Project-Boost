de1c23b7c111f7b18464fc3c4a3cbe0ee2db9eae
Fraser Jubb <fraser.jubb@gmail.com>
2023-09-07 13:45:37 +0100

    Rewritten code for audio slider. Also made it for music only.

--- Assets/Scripts/OptionsMenu.cs
+++ Assets/Scripts/OptionsMenu.cs
@@ -7,55 +7,79 @@ using TMPro;
 
 public class OptionsMenu : MonoBehaviour
 {
-    [SerializeField] private Slider volumeSlider = null; // Allows access to the slider
+    [SerializeField] private Slider musicSlider = null; // Allows access to the slider
     
     [SerializeField] private TextMeshProUGUI volumeTextUI = null; // The number value next to the slider in the UI
     
-    [SerializeField] private AudioMixer Mixer;
-    [SerializeField] private AudioMixMode MixMode;
+    [SerializeField] private AudioMixer MyMixer;
+    // [SerializeField] private AudioMixMode MixMode;
 
-    private void Start() 
+    private void Start()
     {
-        if (PlayerPrefs.HasKey("VolumeValue"))
+        if(PlayerPrefs.HasKey("musicVolume"))
         {
-            LoadValues();
+            LoadVolume();
         } else
-        {    
-            volumeSlider.value = 0.5f; // Value of the slider
-        }
+        SetMusicVolume();
     }
 
-
-    public void OnChangeSlider(float volume) // When we move the slider we pass in a value
+    public void SetMusicVolume()
     {
+        float volume = musicSlider.value;
         volumeTextUI.text = volume.ToString("N1"); // Accessing the text component and it's equal to the paramanter which is converted to a string as can't put float in textbox. M1 = Shows the float with 1 decimal place
-
-        switch (MixMode)
-        {
-            case AudioMixMode.LogrithmicMixerVolume:
-            Mixer.SetFloat("Volume", Mathf.Log10(volume)*20);
-            break;
-        }
-
-        SaveVolumeChanges();
-
+        MyMixer.SetFloat("Music", Mathf.Log10(volume)*20);
+        PlayerPrefs.SetFloat("musicVolume", volume);
     }
 
-    public enum AudioMixMode
+    private void LoadVolume()
     {
-        LogrithmicMixerVolume
+        musicSlider.value = PlayerPrefs.GetFloat("musicVolume");
+
+        SetMusicVolume();
     }
 
-    public void SaveVolumeChanges()
-    {
-        float volumeValue = volumeSlider.value;
-        PlayerPrefs.SetFloat("VolumeValue", volumeValue); // PlayerPrefs.SetFloat(string key, float value);
-    }
+//     private void Start() 
+//     {
+//         if (PlayerPrefs.HasKey("VolumeValue"))
+//         {
+//             LoadValues();
+//         } else
+//         {    
+//             musicSlider.value = 0.5f; // Value of the slider
+//         }
+//     }
 
-    void LoadValues()
-    {
-        float volumeValue = PlayerPrefs.GetFloat("VolumeValue"); // PlayerPrefs.GetFloat(string key)
-        volumeSlider.value = volumeValue;
-        // AudioListener.volume = volumeValue; // Only use this line if want ALL audio linked to this
-    }
+
+//     public void OnChangeSlider(float volume) // When we move the slider we pass in a value
+//     {
+//         volumeTextUI.text = volume.ToString("N1"); // Accessing the text component and it's equal to the paramanter which is converted to a string as can't put float in textbox. M1 = Shows the float with 1 decimal place
+
+//         switch (MixMode)
+//         {
+//             case AudioMixMode.LogrithmicMixerVolume:
+//             MyMixer.SetFloat("Volume", Mathf.Log10(volume)*20);
+//             break;
+//         }
+
+//         SaveVolumeChanges();
+
+//     }
+
+//     public enum AudioMixMode
+//     {
+//         LogrithmicMixerVolume
+//     }
+
+//     public void SaveVolumeChanges()
+//     {
+//         float volumeValue = musicSlider.value;
+//         PlayerPrefs.SetFloat("VolumeValue", volumeValue); // PlayerPrefs.SetFloat(string key, float value);
+//     }
+
+//     void LoadValues()
+//     {
+//         float volumeValue = PlayerPrefs.GetFloat("VolumeValue"); // PlayerPrefs.GetFloat(string key)
+//         musicSlider.value = volumeValue;
+//         // AudioListener.volume = volumeValue; // Only use this line if want ALL audio linked to this
+//     }
 }
